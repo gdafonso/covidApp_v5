549d0dab22e07a05b66385e44d611cb1e9830c5f
fdurante <FDURANTE@ALU.UCAM.EDU>
2020-11-18 10:27:32 +0100

    content provider contactos OK

--- app/src/main/java/com/example/covidapp_v5/ui/content/ContentFragment.java
+++ app/src/main/java/com/example/covidapp_v5/ui/content/ContentFragment.java
@@ -1,9 +1,17 @@
 package com.example.covidapp_v5.ui.content;
 
 import android.Manifest;
+import android.app.AlertDialog;
+import android.content.ContentProviderOperation;
+import android.content.ContentProviderResult;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -27,12 +35,33 @@ public class ContentFragment extends Fragment  {
 
     ArrayAdapter<String> adapter;
     List<String> contactsList = new ArrayList<>();
+    final int Permission_Request = 210;
+    Boolean permissionOperation = false;
+
+    String given_name;
+    String family_name;
+    String mobile;
+    String home;
+    String email;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
+        contactPermission();
+
         View root = inflater.inflate(R.layout.fragment_content, container, false);
 
+        given_name="112 java";
+        family_name="112 java";
+        mobile="112";
+        home="home";
+        email="email";
+
+        if(permissionOperation){
+            addContact(given_name, family_name, mobile, home, email);
+        }else{
+            Toast.makeText(getActivity(), "Permmission required", Toast.LENGTH_LONG).show();
+        }
         ListView contactsView = (ListView)root.findViewById(R.id.contacts_view);
         adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, contactsList);
         contactsView.setAdapter(adapter);
@@ -41,8 +70,58 @@ public class ContentFragment extends Fragment  {
         } else {
             readContacts();
         }
+
+
+
         return root;
     }
+
+    private void addContact(String given_name, String family_name, String mobile, String home, String email) {
+        ArrayList<ContentProviderOperation> contact = new ArrayList<ContentProviderOperation>();
+        contact.add(ContentProviderOperation.newInsert(ContactsContract.RawContacts.CONTENT_URI)
+                .withValue(ContactsContract.RawContacts.ACCOUNT_TYPE, null)
+                .withValue(ContactsContract.RawContacts.ACCOUNT_NAME, null)
+                .build());
+
+        // first and last names
+        contact.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
+                .withValueBackReference(ContactsContract.RawContacts.Data.RAW_CONTACT_ID, 0)
+                .withValue(ContactsContract.RawContacts.Data.MIMETYPE, ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE)
+                .withValue(ContactsContract.CommonDataKinds.StructuredName.GIVEN_NAME, given_name)
+                .withValue(ContactsContract.CommonDataKinds.StructuredName.FAMILY_NAME, family_name)
+                .build());
+
+        // Contact No Mobile
+        contact.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
+                .withValueBackReference(ContactsContract.RawContacts.Data.RAW_CONTACT_ID, 0)
+                .withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE)
+                .withValue(ContactsContract.CommonDataKinds.Phone.NUMBER, mobile)
+                .withValue(ContactsContract.CommonDataKinds.Phone.TYPE, ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE)
+                .build());
+
+        // Contact Home
+        contact.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
+                .withValueBackReference(ContactsContract.RawContacts.Data.RAW_CONTACT_ID, 0)
+                .withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE)
+                .withValue(ContactsContract.CommonDataKinds.Phone.NUMBER, home)
+                .withValue(ContactsContract.CommonDataKinds.Phone.TYPE, ContactsContract.CommonDataKinds.Phone.TYPE_HOME)
+                .build());
+
+        // Email    `
+        contact.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
+                .withValueBackReference(ContactsContract.RawContacts.Data.RAW_CONTACT_ID, 0)
+                .withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.Email.CONTENT_ITEM_TYPE)
+                .withValue(ContactsContract.CommonDataKinds.Email.DATA, email)
+                .withValue(ContactsContract.CommonDataKinds.Email.TYPE, ContactsContract.CommonDataKinds.Email.TYPE_WORK)
+                .build());
+
+        try {
+            ContentProviderResult[] results = getActivity().getContentResolver().applyBatch(ContactsContract.AUTHORITY, contact);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 
     private void readContacts() {
         Cursor cursor = null;
@@ -69,15 +148,71 @@ public class ContentFragment extends Fragment  {
 
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         switch (requestCode) {
-            case 1:
+            case Permission_Request: {
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    readContacts();
+                    // Add your function here which open camera
                 } else {
-                    Toast.makeText(getContext(), "You denied the permission", Toast.LENGTH_SHORT).show();
+                    boolean somePermissionsForeverDenied = false;
+                    for (String permission : permissions) {
+                        if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(), permission)) {
+                            //denied
+                            Log.e("denied", permission);
+                        } else {
+                            if (ActivityCompat.checkSelfPermission(getActivity(), permission) == PackageManager.PERMISSION_GRANTED) {
+                                //allowed
+                                Log.e("allowed", permission);
+                            } else {
+                                //set to never ask again
+                                Log.e("set to never ask again", permission);
+                                somePermissionsForeverDenied = true;
+                            }
+                        }
+                    }
+                    if (somePermissionsForeverDenied) {
+                        final AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(getContext());
+                        alertDialogBuilder.setTitle("Permissions Required")
+                                .setMessage("You have forcefully denied some of the required permissions " +
+                                        "for this action. Please open settings, go to permissions and allow them.")
+                                .setPositiveButton("Settings", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                                                Uri.fromParts("package", getContext().getPackageName(), null));
+                                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                        startActivity(intent);
+                                    }
+                                })
+                                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                    }
+                                })
+                                .setCancelable(false)
+                                .create()
+                                .show();
+                    }
+                    Toast.makeText(getActivity().getApplication(), "Permission required", Toast.LENGTH_LONG).show();
                 }
-                break;
-            default:
+
+                return;
+            }
+        }
+    }
+
+
+    private void contactPermission() {
+        if (android.os.Build.VERSION.SDK_INT >= 23) {
+            // only for gingerbread and newer versions
+            String permission = Manifest.permission.WRITE_CONTACTS;
+            if (ContextCompat.checkSelfPermission(getActivity(), permission) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.WRITE_CONTACTS, Manifest.permission.READ_CONTACTS}, Permission_Request);
+            } else {
+                permissionOperation = true;
+            }
+        } else {
+            permissionOperation = true;
         }
     }
 }
